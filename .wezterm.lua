7039dd9f42bff97523344fd7c12cf35ba5fbf4ee
Jae Shin <mastershin@gmail.com>
2026-02-25 09:55:25 -0700

    Add Alt key mappings for tab activation
    Added key mappings for switching tabs using Alt + number keys.

--- .wezterm.lua
+++ .wezterm.lua
@@ -14,6 +14,16 @@ config.color_scheme = 'Tokyo Night'
 config.window_background_opacity = 0.9
 
 config.keys = {
+    -- Map Alt + 1 through 9 to switch to tabs 0 through 8
+  { key = '1', mods = 'ALT', action = wezterm.action.ActivateTab(0) },
+  { key = '2', mods = 'ALT', action = wezterm.action.ActivateTab(1) },
+  { key = '3', mods = 'ALT', action = wezterm.action.ActivateTab(2) },
+  { key = '4', mods = 'ALT', action = wezterm.action.ActivateTab(3) },
+  { key = '5', mods = 'ALT', action = wezterm.action.ActivateTab(4) },
+  { key = '6', mods = 'ALT', action = wezterm.action.ActivateTab(5) },
+  { key = '7', mods = 'ALT', action = wezterm.action.ActivateTab(6) },
+  { key = '8', mods = 'ALT', action = wezterm.action.ActivateTab(7) },
+  { key = '9', mods = 'ALT', action = wezterm.action.ActivateTab(8) },
   {
     -- Ctrl + Shift + Enter: toggle zoom
     key = 'Enter',
